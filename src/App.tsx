99b361eed87d5aa9a2448fa8dab7947893819de5
TheManWhoCodesStuffSometimes <167024529+TheManWhoCodesStuffSometimes@users.noreply.github.com>
2025-09-09 16:07:08 -0600

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Header from './components/Header';
 import Hero from './components/Hero';
 import Services from './components/Services';
 import Process from './components/Process';
+import CaseStudies from './components/CaseStudies';
 import Team from './components/Team';
 import Testimonials from './components/Testimonials';
 import Contact from './components/Contact';
@@ -19,6 +20,7 @@ function App() {
         <Hero />
         <Services />
         <Process />
+        <CaseStudies />
         <Team />
         <Testimonials />
         <Contact />
